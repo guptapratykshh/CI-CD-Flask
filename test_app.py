0c102c5d79dc9fba6d30f2bd0a0da97d0ccc1ac5
Pratyksh Gupta <pratykshgupta9999@gmail.com>
2025-05-31 17:24:33 +0530

    Added the changes

--- test_app.py
+++ test_app.py
@@ -4,8 +4,3 @@ def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
     assert response.data == b'Hello World. This is Pratyksh Gupta'
-
-def test_health():
-    response = app.test_client().get('/health')
-    assert response.status_code == 200
-    assert response.json == {'status': 'healthy'}
