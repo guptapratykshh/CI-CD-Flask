af19021b144e0168f6af04e92753ef1a7a9527f3
Pratyksh Gupta <pratykshgupta9999@gmail.com>
2025-05-31 17:50:25 +0530

    ADDEd thre changes fior the test

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     response = app.test_client().get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello World. This is Pratyksh Gupta'
+    assert response.data == b"Hello World! This is Pratyksh Gupta's Flask App"
